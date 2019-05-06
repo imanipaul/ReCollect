8bd9ef8b1f492e2e76a0bf2274bd6ca9df24fb19
imanipaul <imanipaul@wustl.edu>
2019-05-05 20:25:16 -0400

    Conditionally render new or existing household registration options

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,31 +1,65 @@
 import React from 'react';
 
 // This component handles our register form
-const Register = (props) => {
+class Register extends React.Component {
 
-    return (
-        <div className="auth-container">
-            <h2>Register</h2>
-            <hr />
-            <form onSubmit={props.handleRegister} >
-                <p>Name:</p>
-                <input name="name" type="text" value={props.formData.name} onChange={props.handleChange} />
-                <p>Password:</p>
-                <input name="password" type="password" value={props.formData.password} onChange={props.handleChange} />
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasHousehold: false
+        }
+    }
 
-                <p>Household</p>
-                <select name='household_id' value={props.formData.household_id} onChange={props.handleChange}>
-                    {props.allHouseholds.map(household => (
-                        <option key={household.id} value={household.id}>{household.name}</option>
-                    ))}
-                </select>
-                <p>Or Create New Household</p>
-                {/* <input name="household_id" type="text" value={props.formData.household_id} onChange={props.handleChange} /> */}
+    render() {
+
+        return (
+            <div className="auth-container">
+                <h2>Register</h2>
                 <hr />
-                <button>Register</button>
-            </form>
-        </div>
-    );
+                <form onSubmit={this.props.handleRegister} >
+                    <p>Name:</p>
+                    <input name="name" type="text" value={this.props.formData.name} onChange={this.props.handleChange} />
+                    <p>Password:</p>
+                    <input name="password" type="password" value={this.props.formData.password} onChange={this.props.handleChange} />
+
+
+                    {
+                        this.state.hasHousehold
+                            ?
+                            <>
+                                <p>Household</p>
+                                <input name='household_id' value={this.props.formData.household_id} onChange={this.props.handleChange} />
+                                <button onClick={() => {
+                                    this.setState({
+                                        hasHousehold: false
+                                    })
+                                }}>Existing Household</button>
+                            </>
+
+                            :
+                            <>
+                                <p>Household</p>
+                                <select name='household_id' value={this.props.formData.household_id} onChange={this.props.handleChange}>
+                                    {this.props.allHouseholds.map(household => (
+                                        <option key={household.id} value={household.id}>{household.name}</option>
+                                    ))}
+                                </select>
+                                <p>Or click here to Create a new household:</p>
+                                <button onClick={() => {
+                                    this.setState({
+                                        hasHousehold: true
+                                    })
+                                }}>New Household</button>
+                            </>
+
+                    }
+
+                    <hr />
+                    <button>Register</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Register;
