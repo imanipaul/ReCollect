8da5bc4e2377d8fef5e6dc6a3e2119e941bb82d2
imanipaul <imanipaul@wustl.edu>
2019-05-09 17:50:17 -0400

    styling

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../stylesheets/LandingPage.css'
-import shopping from '../images/shoppingPNG.png'
+// import shopping from '../images/shoppingPNG.png'
+import cart from '../images/4659.jpg'
 
 
 
 const LandingPage = (props) => {
     return (
         <div className='full-landing-page'>
-            <div className='landing-image'>
-                <img src={shopping} />
-            </div>
+            {/* <div className='landing-image'>
+                <img src={cart} />
+            </div> */}
             <div className='landing-page'>
                 <h1>Welcome to ReCollect</h1>
                 <h3>Never run out of household essentials again</h3>
