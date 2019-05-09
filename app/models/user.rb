9e55cda3cbee38ce0fcdf9bdd193e94794dca4ca
imanipaul <imanipaul@wustl.edu>
2019-05-09 17:54:31 -0400

    Fix merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,7 @@ class User < ApplicationRecord
   belongs_to :household, optional: true
   has_many :items
 
-<<<<<<< HEAD
-  has_secure_password
-=======
   
->>>>>>> ac68ee5f33b1f066ddf0364bc202a427ca57591b
   # validates :password, length: {minimum: 1}, if: -> { new_record? || !password.nil? }
   validates :name, presence: true
 end
